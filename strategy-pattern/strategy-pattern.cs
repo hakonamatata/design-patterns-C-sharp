b1d8b4199657e6346fb9db5620ae8cd7b862e02a
Jan Sviland <jan@sviland.org>
2018-11-30 14:57:30 +0100

    strategy pattern example

--- strategy-pattern/strategy-pattern.cs
+++ strategy-pattern/strategy-pattern.cs
@@ -6,19 +6,59 @@ public class StrategyPattern
   static public void Main()
   {
     Console.WriteLine("hello StrategyPattern!");
+
+    Duck wild = new WildDuck();
+    Duck rubber = new RubberDuck();
+
+    wild.fly();
+    wild.quack();
+
+    rubber.fly();
+    rubber.quack();
+
+
   }
 
 }
 
-public class Duck {
+public interface IQuackBehavior
+{
+  void execute();
+}
 
-  public void display() { }
-  public void quack() { }
+public interface IFlyBehavior
+{
+  void execute();
+}
+
+public class Duck
+{
+
+  IFlyBehavior flyBehavior;
+  IQuackBehavior quackBehavior;
+
+  public Duck(IFlyBehavior flyBehavior, IQuackBehavior quackBehavior) {
+    this.flyBehavior = flyBehavior;
+    this.quackBehavior = quackBehavior;
+  }
+
+  public virtual void fly()
+  {
+    this.flyBehavior.execute();
+  }
+
+  public virtual void quack()
+  {
+    this.quackBehavior.execute();
+  }
 
 }
 
-public class WildDuck : Duck {
-
-
-
+public class NoQuack : IQuackBehavior {
+  
 }
+
+public class flyDefault : IFlyBehavior {
+  
+}
+
