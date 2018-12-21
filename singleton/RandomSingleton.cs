b76e92e99f9a5add5d9492a4fa1541454bfae2e9
Jan Sviland <jan@sviland.org>
2018-12-21 12:17:18 +0100

    singleton pattern

---
+++ singleton/RandomSingleton.cs
@@ -0,0 +1,30 @@
+using System;
+
+/*
+Random class
+
+Represents a pseudo-random number generator, 
+which is a device that produces a sequence of numbers that meet certain statistical requirements for randomness.
+
+The current implementation of the Random class is based on a modified version of Donald E. Knuth's subtractive random number generator algorithm
+
+*/
+class RandomSingleton : Random
+{
+
+  private static RandomSingleton _instance;
+
+  public static RandomSingleton Instance
+  {
+    get
+    {
+
+      if (_instance == null)
+      {
+        _instance = new RandomSingleton();
+      }
+      return _instance;
+    }
+  }
+
+}
