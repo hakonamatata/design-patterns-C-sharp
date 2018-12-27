9b6f9121dfe8facebfb322bfd14f1c9db4be9390
jsviland <sviland.jan@gmail.com>
2018-12-27 16:17:50 +0100

    working on decorator example

--- decorator/decorator-pattern.cs
+++ decorator/decorator-pattern.cs
@@ -14,9 +14,39 @@ public class Solution {
 
   public static void Main(String[] args) {
 
-    
+    Decaf decaf = new Decaf();
+    decaf.getDescription();
+    decaf.getCost();
 
 
   }
+
+}
+
+public abstract class Beverage {
+
+  protected int _cost = 0;
+  protected string _description = "empty";
+
+  public virtual void getCost()  {
+    Console.WriteLine("cost: {0}", _cost);
+  }
+
+  public virtual void getDescription(){
+    Console.WriteLine("description: {0}", _description);
+  }
+
+}
+
+public class Decaf : Beverage {
+
+  public override void getCost() {
+    base._cost += 1;
+    base.getCost();
+  }
+
+}
+
+public class Expresso : Beverage {
 
 }
