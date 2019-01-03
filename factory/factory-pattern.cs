1fd94509175c0ed50d17d08888a4b7def480bf47
Jan Sviland <jan@sviland.org>
2019-01-03 13:29:30 +0100

    factory pattern

--- factory/factory-pattern.cs
+++ factory/factory-pattern.cs
@@ -14,7 +14,58 @@ public class Program
 
   static void Main(string[] args)
   {
+    Console.WriteLine("Hello Factory");
 
+    RandomObstacleCreator factory = new RandomObstacleCreator();
+
+    Console.WriteLine(factory.CreateObstacle());
+
+  }
+
+}
+
+public enum ObstacleType
+{
+    Rock,
+    Ship,
+    Alien
+}
+
+public interface IObstacle
+{
+  ObstacleType GetType();
+}
+
+public class Obstacle : IObstacle
+{
+
+  private string _name;
+
+  public Obstacle(string name)
+  {
+    this._name = name;
+  }
+
+  public override String ToString()
+  {
+    return this._name;
+  }
+
+}
+
+// creator of some shared type
+public interface IObstacleFactory
+{
+  IObstacle CreateObstacle();
+}
+
+// concrete impementation of creator that initialize type
+public class RandomObstacleCreator : IObstacleFactory
+{
+
+  public IObstacle CreateObstacle()
+  {
+    return new Obstacle("Large Rock");
   }
 
 }
