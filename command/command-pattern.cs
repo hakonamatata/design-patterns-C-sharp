e58f729f0ea8d7914b7e5bb00b1b1aff55d77217
Jan Sviland <jan@sviland.org>
2019-01-22 11:23:34 +0100

    working on command pattern

--- command/command-pattern.cs
+++ command/command-pattern.cs
@@ -1,11 +1,50 @@
 using System;
 
+/*
+
+Encapsulate a command that is sent between objects. 
+
+Allow easily undo and redo functionality, by adding undo function to command.  
+Allows for batching multple commands / actions by executing a command that consist of multiple commands.
+
+*/
+
 public class Program
 {
-  
+
   static void Main(string[] args)
   {
     Console.WriteLine("Hello world");
   }
+
+}
+
+// any command can act upon receiver
+class Receiver
+{
+
+}
+
+// invoker has one to many commands
+// invoker act upon receiver via commands
+class Invoker
+{
+  ICommand[] commands;
+
+  void setCommand(ICommand c)
+  {
+
+  }
+
+}
+
+interface ICommand
+{
+  void execute();
+  void undo();
+}
+
+class Command : ICommand
+{
 
 }
